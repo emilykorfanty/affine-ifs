f898bb04fd96498585ced0b001cf14dfb5125020
Emily Rose Korfanty <52767963+emilykorfanty@users.noreply.github.com>
2019-07-11 14:53:31 -0700

    Update KocheSnowflake.R

--- KocheSnowflake.R
+++ KocheSnowflake.R
@@ -7,7 +7,7 @@
 # 
 # Setup 
 #-----------------------------------------
-N <- 10000  # Number of iterations
+N <- 100000  # Number of iterations
 
 # scale
 lambda <- list(1/3, 1/3, 1/3, 1/3, sqrt(3)/3, 1/3, 1/3)
@@ -18,26 +18,22 @@ theta <- list(-2*pi/3, pi/3, 0, -pi/3, pi/6, pi, 0)
 # rotation matrices 
 R <- list()
 for(i in 1:length(theta)){
-R[[i]] <- matrix(data=c(cos(theta[[i]]), sin(theta[[i]]), -sin(theta[[i]]), cos(theta[[i]])), nrow = 2, ncol = 2, byrow = TRUE)
+  R[[i]] <- matrix(data=c(cos(theta[[i]]), sin(theta[[i]]), -sin(theta[[i]]), cos(theta[[i]])), nrow = 2, ncol = 2, byrow = TRUE)
 }
 
 # shifts 
 V <- list(c(1/6, sqrt(3)/6), c(1/6, sqrt(3)/6), c(1/3, sqrt(3)/3), c(2/3, sqrt(3)/6), c(1/3, 0), c(2/3, 0), c(2/3, 0)) 
 
-# the ifs
-FF <- list()
-for(i in 1:length(theta)){
-  FF[[i]] <- function(input_vector){
-    output_vector <- c(lambda[[i]]*unlist(R[[i]])%*%input_vector + unlist(V[[i]]))
-    return(output_vector)
-    }
-}
+# the ifs, FF(i, x) = f_i(x)
+FF <- function(i, input_vector){
+  output_vector <- c(unlist(lambda[[force(i)]])*unlist(R[[force(i)]])%*%input_vector + unlist(V[[force(i)]]))
+  return(output_vector)}
 
 # Decisions for which function to use in each iteration 
 # P(v_i) = 1/8 for 1 = 1, ..., 8
 D <- sample(c(1,2,3,4,5,6,7),N,replace = TRUE)
 
-X <- list() # A list to hold the points of the gasket
+X <- list() # A list to holMd the points of the gasket
 
 # Starting point
 x_0 <- 1
@@ -46,7 +42,7 @@ X[[1]] <- c(x_0,y_0)
 
 # Apply the functions, in the order of D, to (x_0, y_0)
 for(n in 2:N){
-  X[[n]] <- FF[[D[[n-1]]]](X[[n-1]])
+  X[[n]] <- FF(unlist(D[[n-1]]), unlist(X[[n-1]]))
 }
 
 # Prepare for plotting
