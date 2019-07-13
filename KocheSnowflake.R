b81b120d4ba953f2ae59b91b941c625ad645a3fa
Emily Rose Korfanty <52767963+emilykorfanty@users.noreply.github.com>
2019-07-13 16:21:10 -0700

    Update KocheSnowflake.R

--- KocheSnowflake.R
+++ KocheSnowflake.R
@@ -1,5 +1,5 @@
-# With some rotations - Random Iteration Method
-# Koche Snowflake
+# Koche Snowflake - Random Iteration Method
+# 
 #-----------------------------------------
 #-----------------------------------------
 # IFS:
@@ -10,34 +10,34 @@
 N <- 100000  # Number of iterations
 
 # scale
-lambda <- list(1/3, 1/3, 1/3, 1/3, sqrt(3)/3, 1/3, 1/3)
+lambda <- list(1/sqrt(3), 1/3, 1/3, 1/3, 1/3, 1/3, 1/3)
 
 # rotation angles
-theta <- list(-2*pi/3, pi/3, 0, -pi/3, pi/6, pi, 0)  
+theta <- list(pi/6, 0, 0, 0, 0, 0, 0)  
 
 # rotation matrices 
 R <- list()
 for(i in 1:length(theta)){
-  R[[i]] <- matrix(data=c(cos(theta[[i]]), sin(theta[[i]]), -sin(theta[[i]]), cos(theta[[i]])), nrow = 2, ncol = 2, byrow = TRUE)
+  R[[i]] <- matrix(data=c(cos(theta[[i]]), -sin(theta[[i]]), sin(theta[[i]]), cos(theta[[i]])), nrow = 2, ncol = 2, byrow = TRUE)
 }
 
 # shifts 
-V <- list(c(1/6, sqrt(3)/6), c(1/6, sqrt(3)/6), c(1/3, sqrt(3)/3), c(2/3, sqrt(3)/6), c(1/3, 0), c(2/3, 0), c(2/3, 0)) 
+V <- list(c(0, 0), c(1/sqrt(3), 1/3), c(0, 2/3), c(-1/sqrt(3), 1/3), c(-1/sqrt(3), -1/3), c(0, -2/3), c(1/sqrt(3), -1/3)) 
 
 # the ifs, FF(i, x) = f_i(x)
 FF <- function(i, input_vector){
-  output_vector <- c(unlist(lambda[[force(i)]])*unlist(R[[force(i)]])%*%input_vector + unlist(V[[force(i)]]))
+  output_vector <- c(unlist(lambda[[i]])*unlist(R[[i]])%*%input_vector + unlist(V[[i]]))
   return(output_vector)}
 
 # Decisions for which function to use in each iteration 
-# P(v_i) = 1/8 for 1 = 1, ..., 8
+# P(v_i) = 1/7 for 1 = 1, ..., 7
 D <- sample(c(1,2,3,4,5,6,7),N,replace = TRUE)
 
 X <- list() # A list to holMd the points of the gasket
 
 # Starting point
-x_0 <- 1
-y_0 <- 1
+x_0 <- 0
+y_0 <- 0
 X[[1]] <- c(x_0,y_0)
 
 # Apply the functions, in the order of D, to (x_0, y_0)
@@ -54,3 +54,4 @@ for(i in 2:N){
 # Plot the points
 plot(x=M[,1],y=M[,2],
      xlab ="",ylab="",pch='.',frame.plot=FALSE)
+
